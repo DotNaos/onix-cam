a49811ffe8da6150811b6404bfaa91dfd44829d0
DotNaos <schuetzoliver00@gmail.com>
2023-10-27 08:36:35 +0200

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import NextLink from "next/link";
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code"
+import { button as buttonStyles } from "@nextui-org/theme";
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function Home() {
+	return (
+    <div> 
+    </div>
+  );
+}
